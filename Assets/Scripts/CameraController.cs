90bd72f0c4263543d562387378bfc0117ad22453
YenielDiaz <yenie@LAPTOP-KARDRO8G>
2020-10-17 01:11:07 -0400

    Implemented Scrolling Background
    Added Scene to test scrolling background
    Added placeholder background art
    Added script to have inifinite parallax scrolling background
    Added Folders for organization purposes
    Added Camera Controller to test moving background

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        //test code to move the camera automatically
+        transform.position += new Vector3(0.1f,0,0) * speed;
+    }
+}
---
+++ Assets/Scripts/ParallaxScroll.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScroll : MonoBehaviour
+{
+    private float length, startPos;
+    private Camera cam;
+
+    [SerializeField] private float parallaxFactor;
+    [SerializeField] private float offset;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        cam = Camera.main;
+    }
+
+
+    void Update()
+    {
+        //get distance traveled relative to the camera
+        float distRelativeToCamera = cam.transform.position.x * (1 - parallaxFactor);
+        //get distance we will add to its position
+        float distance = cam.transform.position.x * parallaxFactor;
+
+        //move background part based on how much parallaxFactor it has
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if(distRelativeToCamera > startPos + (length - offset))
+        {
+            //update start position to the right so that the camera does not go out of bounds
+            startPos += length;
+        }
+        else if (distRelativeToCamera < startPos - (length - offset))
+        {
+            //update start position to the left so that the camera does not go out of bounds
+            startPos -= length;
+        }
+    }
+}
