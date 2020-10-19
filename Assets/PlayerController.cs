7b41030a851e3c3589d51452b31ab99da3fd3d24
YenielDiaz <yenie@LAPTOP-KARDRO8G>
2020-10-19 01:17:04 -0400

    Added Player Sprite and Some Folders
    Added prefab folder
    Added animations folder
    Added Player Sprites and Gameobject
    Added Enemy Game Object

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(0.1f, 0, 0) * speed;
+    }
+}
